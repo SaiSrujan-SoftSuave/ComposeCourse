8c8f72be56fee0677ffea78ddf637d97dd14aa21
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2024-05-27 16:01:28 +0530

    added remember

--- app/src/main/java/com/example/composecourse/MainActivity.kt
+++ app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -9,8 +9,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,11 +32,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count :MutableState<Int> = remember {
+        var count  by remember {
             mutableIntStateOf(0)
         }
-        Text("You've had ${count.value} glasses.")
-        Button(onClick = { count.value ++ }, Modifier.padding(top = 8.dp)) {
+        Text("You've had ${count} glasses.")
+        Button(onClick = { count ++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
