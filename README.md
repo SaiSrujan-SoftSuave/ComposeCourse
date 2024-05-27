940e2377f8fab16801e9d3156201f01a50ff89fc
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2024-05-27 15:57:57 +0530

    remeber handles compostion of ui

--- README.md
+++ README.md
@@ -19,3 +19,43 @@ Events are inputs generated from outside or inside an application, such as:
 events are the mechanism through which the state changes, resulting in changes to the UI.**
 
 ![img.png](img.png)
+
+## Memory in a composable function
+
+- **The Composition:** a description of the UI built by Jetpack Compose when it executes composables.
+
+- **Initial composition:** creation of a Composition by running composables the first time.
+
+- **Recomposition:** re-running composables to update the Composition when data changes.
+
+**Compose has a special state tracking system in place that schedules recompositions for any composables that read a particular state.**
+
+Use Compose's **State** and **MutableState** types to make state observable by Compose.
+
+```kotlin
+ Column(modifier = modifier.padding(16.dp)) {
+       // Changes to count are now tracked by Compose
+       val count: MutableState<Int> = mutableStateOf(0)
+
+       Text("You've had ${count.value} glasses.")
+        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
+           Text("Add one")
+       }
+   }
+```
+
+even this not work because
+Scheduling recompositions is working fine. However, when a recomposition happens, the variable count is re-initialized back to 0, so we need a way to preserve this value across recompositions.
+
+## Remember
+
+the remember composable inline function. A value calculated by remember is stored in the Composition during the initial composition, and the stored value is kept across recompositions.
+
+**You can think of using remember as a mechanism to store a single object in the Composition, in the same way a private val property does in an object.**
+
+```kotlin
+val count: MutableState<Int> = remember{ mutableStateOf(0) }
+
+//use delegates like 'by' remove .value usage
+var count by remember { mutableStateOf(0) }
+```
--- app/src/main/java/com/example/composecourse/MainActivity.kt
+++ app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -3,63 +3,17 @@ package com.example.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.StringRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.material3.windowsizeclass.WindowSizeClass
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationRail
-import androidx.compose.material3.NavigationRailItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composecourse.Constants.alignYourBodyData
-import com.example.composecourse.Constants.favoriteCollectionsData
 import com.example.composecourse.ui.theme.ComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -76,9 +30,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text("You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        var count :MutableState<Int> = remember {
+            mutableIntStateOf(0)
+        }
+        Text("You've had ${count.value} glasses.")
+        Button(onClick = { count.value ++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
