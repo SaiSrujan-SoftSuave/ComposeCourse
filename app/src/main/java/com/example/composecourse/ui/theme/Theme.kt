93fab17590b84945b7123fc27bfb23a149745c20
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2024-06-03 15:28:53 +0530

    project setup

--- app/src/main/java/com/example/composecourse/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composecourse/ui/theme/Theme.kt
@@ -1,15 +1,11 @@
 package com.example.composecourse.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
 //    primary = md_theme_dark_primary,
@@ -104,19 +100,15 @@ fun ComposeCourseTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
+    val colors = lightColorScheme(
+        primary = Melon,
+        primaryContainer = PaleDogwood,
+        onPrimary = Color.Black,
+        secondary = Peach,
+        onSecondary = Color.Black
+    )
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = typography,
+        colorScheme = colors,
         content = content
     )
 }
